23e2d700f9c2df3b36c82cfe3b23a388da12f3fe
lucaliberti <liberti.luca@gmail.com>
2022-02-13 20:37:35 +0100

    finito

---
+++ src/app/genera-salva01/genera-salva01.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { MyserviceService } from '../myservice.service';
+
+
+@Component({
+  selector: 'app-genera-salva01',
+  templateUrl: './genera-salva01.component.html',
+  styleUrls: ['./genera-salva01.component.css']
+})
+export class GeneraSalva01Component implements OnInit {
+
+  mydata:number=0
+
+  constructor( private service:MyserviceService) { }
+  
+  ngOnInit(): void {
+    this.mydata=this.service.readNumber()
+  }
+
+  // ------------------------------
+  // gestione degli eventi
+  onLeggiNuovoDato(){
+    this.mydata=this.service.readNumber()
+  }
+
+  onGeteraNuovoDato(){
+    this.service.genNewNumber()
+  }
+
+}
